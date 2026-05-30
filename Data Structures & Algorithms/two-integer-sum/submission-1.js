d30c6877684458344fba5c1db6faac48cc58df91
Anul Jain <anuljain23@gmail.com>
2026-05-30 22:16:21 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for(let i = 0; i < nums.length; i++) {
+            if (nums.includes(target - nums[i]) && i !== nums.indexOf(target-nums[i])) {
+                return [i, nums.indexOf(target-nums[i])];
+            }
+        }
+    }
+}
